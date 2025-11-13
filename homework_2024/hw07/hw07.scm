9b0822c5609f877f6367e55f56a9dbac66e123d8
ma-jiale <majialea@126.com>
2025-11-13 15:07:47 +0800

    finished hwk07

---
+++ homework_2024/hw07/hw07.scm
@@ -0,0 +1,26 @@
+(define (square n) (* n n))
+
+(define (pow base exp) 
+  (if (= exp 1) 
+    base
+    (if (even? exp) 
+      (square (pow base (/ exp 2))) 
+      (* base (square (pow base (/ (- exp 1) 2))))
+    )
+  )
+)
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (begin (define y (* x x x)) 
+        (repeatedly-cube (- n 1) y)
+      )
+  )
+)
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cddr s)))
