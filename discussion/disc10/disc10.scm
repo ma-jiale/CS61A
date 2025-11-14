fd435c31f64792bf8a5965ed1c74806e4d794161
ma-jiale <majialea@126.com>
2025-11-14 13:31:10 +0800

    finish disc10

---
+++ discussion/disc10/disc10.scm
@@ -0,0 +1,47 @@
+(define (factorial x)
+    (if (= x 1) 
+        1
+        (* x (factorial (- x 1)))
+    )
+)
+
+(define (fib n)
+    (if (or (= n 1) (= n 2))
+        1
+        (+ (fib (- n 1)) (fib (- n 2)))
+    )
+)
+
+(define (my-append a b)
+    (define (helper a)
+        (if (equal? a nil)
+            b
+            (cons (car a) (helper (cdr a)))
+        )
+    )
+    (helper a)
+)
+
+(define (duplicate lst)
+    (if (equal? lst nil)
+        nil
+        (cons (car lst) (cons (car lst) (duplicate (cdr lst))))
+    )
+)
+
+(define (insert element lst index)
+    (if (equal? (cdr lst) nil)
+        (cond
+            ((= index 0) (list element (car lst)))
+            ((= index 1) (list (car lst) element))
+        )
+        (if (= index 0)
+            (cons element (cons (car lst) (cdr lst)))
+            (cons (car lst) (insert element (cdr lst) (- index 1)))
+        )
+    )
+)
+    
+    
+        
+        
